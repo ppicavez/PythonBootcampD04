d0b7277d7e77a0c986af7ba7419b813987fa5b00
ppicavez <ppicavez@student.42lyon.fr>
2020-05-29 21:49:16 +0200

    ex02 done

--- ex02/ProportionBySport.py
+++ ex02/ProportionBySport.py
@@ -6,13 +6,13 @@ def proportionBySport(df, olympic_year, sport, gender):
     mask = (df["Year"] == olympic_year) & (df["Sex"] == gender)\
         & (df['Sport'] == sport)
     df_selected = df[mask]
-    df_selected = df_selected.drop_duplicates(subset =['ID'])
+    df_selected = df_selected.drop_duplicates(subset=['ID'])
     nb_selected = len(df_selected)
     print(nb_selected)
 
     mask = (df["Year"] == olympic_year) & (df["Sex"] == gender)
     df_selected = df[mask]
-    df_selected = df_selected.drop_duplicates(subset =['ID'])
+    df_selected = df_selected.drop_duplicates(subset=['ID'])
     nb_global_selected = len(df_selected)
     print(nb_global_selected)
 
